c2bbb7f943608ebbd5dc35637bc99503ee7f50e6
beckyo6 <rebtshikadila@gmail.com>
2024-01-19 10:28:13 +0200

    add flowbite plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [require("tailwindcss-animate"),
-],
+  plugins: [require("tailwindcss-animate"), require('flowbite/plugin')
+
+  ],
 }
 
