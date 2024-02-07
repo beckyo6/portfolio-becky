be0e5de48d51f05aef3fc32384d930a62a76f613
beckyo6 <rebtshikadila@gmail.com>
2024-02-07 23:13:47 +0200

    config raleway font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
+    fontFamily: {
+      raleway: ["Raleway", "sans-serif"],
+    },
     extend: {},
-    
+
   },
   plugins: [require('flowbite/plugin')
 
