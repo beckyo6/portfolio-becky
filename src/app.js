7ebf5fdeb932bfde777c779d64938a57986e85aa
beckyo6 <rebtshikadila@gmail.com>
2024-02-07 18:55:01 +0200

    add animation in general

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const ratio = .1
+const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: ratio,
+};
+
+const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
+        } 
+    });
+}
+
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll(".reveal").forEach(function(r){
+    observer.observe(r)
+})
