ced2393678cf8423370f51dda6e5a02061b9c3ef
beckyo6 <rebtshikadila@gmail.com>
2024-10-14 21:05:33 +0200

    when on mobile, the menu is displayed vertically

--- src/app.js
+++ src/app.js
@@ -9,9 +9,10 @@ const handleIntersect = function (entries, observer) {
     entries.forEach(function (entry) {
         if (entry.intersectionRatio > ratio) {
             entry.target.classList.add('reveal-visible')
-            // observer.unobserve(entry.target)
+                // observer.unobserve(entry.target)
 
-+[];        }
+                + [];
+        }
         else {
             entry.target.classList.remove('reveal-visible')
             // observer.unobserve(entry.target)
@@ -20,6 +21,14 @@ const handleIntersect = function (entries, observer) {
 }
 
 const observer = new IntersectionObserver(handleIntersect, options);
-document.querySelectorAll('[class*="reveal-"]').forEach(function(r){
+document.querySelectorAll('[class*="reveal-"]').forEach(function (r) {
     observer.observe(r)
 })
+
+// script sur le button burger
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+});
